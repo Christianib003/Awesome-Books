c6a72d84096cf3bc074f11329ccc3cc61f25800e
benshidanny11 <benshidanny11@gmail.com>
2022-04-19 11:11:42 +0200

    Refactor project(Change plain JS objects to classes)

--- js/main.js
+++ js/main.js
@@ -1,24 +1,36 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
+
+class Book {
+  constructor(id, title, author) {
+    this.id = id;
+    this.title = title;
+    this.author = author;
+  }
+}
 const books = JSON.parse(localStorage.getItem('books')) || [];
+
+const removeBook = (id) => {
+  const mBooks = JSON.parse(localStorage.getItem('books'));
+  const remainingBooks = mBooks.filter((book) => book.id !== id);
+  const removedBooks = JSON.stringify(remainingBooks);
+  localStorage.setItem('books', removedBooks);
+  window.location.reload();
+};
 
 const bookList = document.querySelector('#book-list');
 let html = '';
 books.forEach((book) => {
-  html += `<li id="book-item">
-<article class="article-container">
-  <h3 class="title">${book.title}</h3>
-  <h3 class="title">${book.author}</h3>
+  html += `<li id="book-item" class="article">
+  <h3 class="title">${book.title} by ${book.author}</h3>
   <button
     class="book-remove"
     data-book-id="${book.id}"
     id="remove-button"
-    onclick="removeBook(${book.id})"
+    onClick="removeBook(${book.id})"
   >
     Remove
   </button>
-  <hr>
-</article>
 </li>`;
 });
 bookList.innerHTML = html;
@@ -28,17 +40,9 @@ document.getElementById('addbook').addEventListener('click', () => {
   const author = document.querySelector('#author').value;
   if (title.length > 0 && author.length > 0) {
     const id = new Date().getTime();
-    const book = { id, author, title };
+    const book = new Book(id, title, author);
     books.push(book);
     localStorage.setItem('books', JSON.stringify(books));
     location.reload();
   }
 });
-
-const removeBook = (id) => {
-  const mBooks = JSON.parse(localStorage.getItem('books'));
-  const remainingBooks = mBooks.filter((book) => book.id !== id);
-  const removedBooks = JSON.stringify(remainingBooks);
-  localStorage.setItem('books', removedBooks);
-  window.location.reload();
-};
