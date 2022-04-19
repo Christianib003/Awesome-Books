513ae31a82feb68620a850e0ca52e31bb05e7de2
benshidanny11 <benshidanny11@gmail.com>
2022-04-19 18:37:36 +0200

    Resolve feedback from reviewer

--- js/main.js
+++ js/main.js
@@ -1,20 +1,16 @@
 /* eslint-disable class-methods-use-this */
-/* eslint-disable no-unused-vars */
 /* eslint-disable no-restricted-globals */
-const books = JSON.parse(localStorage.getItem('books')) || [];
-class Book {
-  constructor(id = null, title = null, author = null) {
-    this.id = id;
-    this.title = title;
-    this.author = author;
+class Books {
+  constructor() {
+    this.books = JSON.parse(localStorage.getItem('books')) || [];
   }
 
   addBook(title, author) {
     if (title.length > 0 && author.length > 0) {
       const id = new Date().getTime();
-      const book = new Book(id, title, author);
-      books.push(book);
-      localStorage.setItem('books', JSON.stringify(books));
+      const book = { id, title, author };
+      this.books.push(book);
+      localStorage.setItem('books', JSON.stringify(this.books));
       location.reload();
     }
   }
@@ -29,15 +25,13 @@ class Book {
 
   listAllBooks() {
     const bookList = document.querySelector('#book-list');
-    books.forEach((book) => {
+    this.books.forEach((book) => {
       const listItem = document.createElement('li');
       listItem.setAttribute('id', 'book-item');
       listItem.classList.add('article');
-
       const bookData = document.createElement('h3');
       bookData.classList.add('title');
       bookData.innerHTML = `${book.title} by ${book.author}`;
-
       const btnRemove = document.createElement('button');
       btnRemove.innerHTML = 'Remove book';
       btnRemove.classList.add('book-remove');
@@ -54,8 +48,8 @@ class Book {
 document.getElementById('addbook').addEventListener('click', () => {
   const title = document.querySelector('#title').value;
   const author = document.querySelector('#author').value;
-  const book = new Book();
+  const book = new Books();
   book.addBook(title, author);
 });
 
-new Book().listAllBooks();
+new Books().listAllBooks();
