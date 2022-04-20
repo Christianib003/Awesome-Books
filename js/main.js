17c4becdb6b7fb565d742f440b71089c8a27c13e
benshidanny11 <benshidanny11@gmail.com>
2022-04-20 21:27:37 +0200

    Implemt feedback from reveiwer

--- js/main.js
+++ js/main.js
@@ -30,6 +30,7 @@ class Books {
       const listItem = document.createElement('li');
       listItem.setAttribute('id', 'book-item');
       listItem.classList.add('article-container');
+      listItem.classList.add('d-flex');
       const bookData = document.createElement('h3');
       bookData.classList.add('title');
       bookData.innerHTML = `${book.title} by ${book.author}`;
@@ -65,8 +66,10 @@ document.getElementById('list-books-item').addEventListener('click', () => {
   list.classList.remove('hide');
   const section = document.getElementById('section-addbook');
   section.classList.add('hide');
+  section.classList.remove('d-flex');
   const contact = document.getElementById('contact-section');
   contact.classList.add('hide');
+  contact.classList.remove('d-flex');
   document.getElementById('add-books-item').classList.remove('active');
   document.getElementById('list-books-item').classList.add('active');
   document.getElementById('add-contact-item').classList.remove('active');
@@ -75,23 +78,30 @@ document.getElementById('list-books-item').addEventListener('click', () => {
 document.getElementById('add-books-item').addEventListener('click', () => {
   const sectionAddBook = document.getElementById('section-addbook');
   sectionAddBook.classList.remove('hide');
+  sectionAddBook.classList.add('d-flex');
   document.getElementById('add-books-item').classList.add('active');
   document.getElementById('list-books-item').classList.remove('active');
   document.getElementById('add-contact-item').classList.remove('active');
   const bookContainer = document.getElementById('book-container');
   bookContainer.classList.add('hide');
+  bookContainer.classList.remove('d-flex');
   const contact = document.getElementById('contact-section');
   contact.classList.add('hide');
+  contact.classList.remove('d-flex');
 });
 
 document.getElementById('add-contact-item').addEventListener('click', () => {
   const sectionAddBook = document.getElementById('section-addbook');
   sectionAddBook.classList.add('hide');
+  sectionAddBook.classList.remove('d-flex');
   document.getElementById('add-books-item').classList.remove('active');
   document.getElementById('list-books-item').classList.remove('active');
   document.getElementById('add-contact-item').classList.add('active');
   const bookContainer = document.getElementById('book-container');
   bookContainer.classList.add('hide');
+  bookContainer.classList.remove('d-flex');
+
   const contact = document.getElementById('contact-section');
   contact.classList.remove('hide');
+  contact.classList.add('d-flex');
 });
